2a0b86ada913562960a3559fa54ee83bfd632552
Guarionex <edwinom@iastate.edu>
2015-04-21 16:30:57 -0500

    Completed internal vertex and edge in graph

--- ComS311HW7Edwin/src/DirectedGraph.java
+++ ComS311HW7Edwin/src/DirectedGraph.java
@@ -1,7 +1,80 @@
+import java.util.HashMap;
 import java.util.Set;
 
 
 public class DirectedGraph<V, E> implements Graph<V, E> {
+	
+	private int numVertices = 0;
+	private int numEdges = 0;
+	
+	private HashMap<Integer, GraphVertex> vertices = new HashMap<Integer, GraphVertex>();
+	private HashMap<Integer, GraphEdge> edges = new HashMap<Integer, GraphEdge>();
+	
+	class GraphEdge
+	{
+		private E edgeData;
+		private int sourceID;
+		private int targetID;
+		private int ID;
+		
+		public GraphEdge(E edgeData, int sourceID, int targetID, int ID)
+		{
+			this.edgeData = edgeData;
+			this.sourceID = sourceID;
+			this.targetID = targetID;
+			this.ID = ID;
+		}
+		
+		public E getEdge()
+		{
+			return edgeData;
+		}
+		
+		public int getSourceID()
+		{
+			return sourceID;
+		}
+		
+		public int getTargetID()
+		{
+			return targetID;
+		}
+		
+		@Override
+		public int hashCode()
+		{
+			return ID;
+		}
+	}
+	
+	class GraphVertex
+	{
+		private V vertexData;
+		private int ID;
+		private HashMap<Integer, GraphEdge> edges = new HashMap<Integer, GraphEdge>();
+		
+		public GraphVertex(V vertexData)
+		{
+			this.vertexData = vertexData;
+		}
+		
+		public V getData()
+		{
+			return vertexData;
+		}
+		
+		public void addEdge(GraphEdge edge)
+		{
+			edges.put(edge.hashCode(), edge);
+		}
+		
+		@Override
+		public int hashCode()
+		{
+			return ID;
+		}
+		
+	}
 
 	@Override
 	public int addVertex(V v) {
