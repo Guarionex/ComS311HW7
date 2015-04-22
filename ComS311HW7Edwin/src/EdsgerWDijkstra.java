60bb9308eeda165a6caf60f326e3c6d1d5f2a767
Guarionex <edwinom@iastate.edu>
2015-04-21 19:19:41 -0500

    Implementing computeShortestPath

--- ComS311HW7Edwin/src/EdsgerWDijkstra.java
+++ ComS311HW7Edwin/src/EdsgerWDijkstra.java
@@ -1,30 +1,56 @@
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 
 public class EdsgerWDijkstra<V, E> implements Dijkstra<V, E> {
 
+	private Graph<V, E> graph = null;
+	private int startVertex = -1;
+	private Weighing<E> weighing = null;
+	private HashMap<Integer, Double> distance;
+	private HashMap<Integer, Integer> predecessor;
+	private HashSet<Integer> vertexSet;
+	
+	public EdsgerWDijkstra()
+	{
+		distance = new HashMap<Integer, Double>();
+		predecessor = new HashMap<Integer, Integer>();
+		vertexSet = new HashSet<Integer>();
+	}
+	
 	@Override
 	public void setGraph(Graph<V, E> graph) {
-		// TODO Auto-generated method stub
-		
+		this.graph = graph;
 	}
 
 	@Override
-	public void setStart(int startId) throws IllegalArgumentException,
-			IllegalStateException {
-		// TODO Auto-generated method stub
-		
+	public void setStart(int startId) throws IllegalArgumentException, IllegalStateException {
+		if(graph == null) throw new IllegalStateException();
+		if(!graph.getVertices().contains(startId)) throw new IllegalArgumentException();
+		else startVertex = startId;
+
 	}
 
 	@Override
 	public void setWeighing(Weighing<E> weighing) {
-		// TODO Auto-generated method stub
+		this.weighing = weighing;
 		
 	}
 
 	@Override
 	public void computeShortestPath() throws IllegalStateException {
-		// TODO Auto-generated method stub
+		if(graph == null || startVertex == -1 || weighing == null) throw new IllegalStateException();
+		
+		distance = new HashMap<Integer, Double>();
+		predecessor = new HashMap<Integer, Integer>();
+		vertexSet = new HashSet<Integer>();
+		
+		for(Integer v : graph.getVertices())
+		{
+			distance.put(v, Double.POSITIVE_INFINITY);
+			predecessor.put(v,  null);
+		}
 		
 	}
 
