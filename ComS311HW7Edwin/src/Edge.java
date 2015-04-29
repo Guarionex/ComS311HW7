e0b258294f27a253864391e95759785feec89817
Guarionex <edwinom@iastate.edu>
2015-04-28 23:51:57 -0500

    Updated Edge to include name (optional)

--- ComS311HW7Edwin/src/Edge.java
+++ ComS311HW7Edwin/src/Edge.java
@@ -5,6 +5,7 @@ public class Edge {
 	private Vertex previous;
 	private Vertex next;
 	private double weight;
+	private String name;
 	
 	public Edge(int ID, Vertex previous, Vertex next, double weight)
 	{
@@ -12,6 +13,16 @@ public class Edge {
 		this.previous = previous;
 		this.next = next;
 		this.weight = weight;
+		this.name = "";
+	}
+	
+	public Edge(int ID, Vertex previous, Vertex next, double weight, String name)
+	{
+		this.ID = ID;
+		this.previous = previous;
+		this.next = next;
+		this.weight = weight;
+		this.name = name;
 	}
 
 	public int getID()
@@ -32,5 +43,10 @@ public class Edge {
 	public double getWeight()
 	{
 		return weight;
+	}
+	
+	public String getName()
+	{
+		return name;
 	}
 }
