1b3d341795dfe5ba4c6a8c6031767619d3b8bc44
Guarionex <edwinom@iastate.edu>
2015-04-30 01:17:32 -0500

    Part 1 done

--- ComS311HW7Edwin/src/AmesCoffeeGrapher.java
+++ ComS311HW7Edwin/src/AmesCoffeeGrapher.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.List;
 
 
 public class AmesCoffeeGrapher {
@@ -9,16 +10,69 @@ public class AmesCoffeeGrapher {
 	{
 		GraphGen cartographer = new GraphGen("AmesData.txt");
 		Graph<Vertex, Edge> ames = null;
+		Graph<Vertex, Edge> locations = null;
+		CoffeeSolver<Vertex, Edge> coffeeSolver = new CoffeeSolution<Vertex, Edge>();
+		
 		try {
 			ames = cartographer.makeGraph();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Vertex count is "+ames.getVertices().size());
-		System.out.println("Edge count is "+ames.getEdges().size());
+//		System.out.println("Vertex count is "+ames.getVertices().size());
+//		System.out.println("Edge count is "+ames.getEdges().size());
+		
+		locations = cartographer.makeLocationGraph();
+		
+//		Part 1: Generate a sorting of the ingredients that satisfies the constraints
+//		above. Note that you expect Jim to have this problem again, therefore
+//		your algorithm must work for an arbitrary dependency graph of
+//		arbitrary size.
+		List<Integer> topo = coffeeSolver.sortVertices(locations);
+		System.out.println("Sorting of the ingredients that statisfies the constraint");
+		//System.out.println(topo);
+		System.out.println(ingredientToString(topo));
+
+		
 	}
 	
+	private static String ingredientToString(List<Integer> order)
+	{
+		String sortOrder = "";
+		
+		for(Integer v: order)
+		{
+			switch(v)
+			{
+			case 0:
+				sortOrder +="You -> ";
+				break;
+			case 1:
+				sortOrder += "Ingredient A -> ";
+				break;
+			case 2:
+				sortOrder += "Ingredient B -> ";
+				break;
+			case 3:
+				sortOrder += "Ingredient C -> ";
+				break;
+			case 4:
+				sortOrder += "Ingredient D -> ";
+				break;
+			case 5:
+				sortOrder += "Ingredient E -> ";
+				break;
+			case 6:
+				sortOrder += "Ingredient F -> ";
+				break;
+			case 7:
+				sortOrder += "Ingredient G";
+				break;
+			}
+		}
+		
+		return sortOrder;
+	}
 	
 	
 }
--- ComS311HW7Edwin/src/GraphGen.java
+++ ComS311HW7Edwin/src/GraphGen.java
@@ -66,5 +66,55 @@ public class GraphGen {
 		
 		return graph;
 	}
+	
+	public Graph<Vertex, Edge> makeLocationGraph()
+	{
+		Graph<Vertex, Edge> locations = new DirectedGraph<Vertex, Edge>();
+		Vertex A = new Vertex(1055, 42.059112, -93.632995);
+		Vertex B = new Vertex(371, 42.019621, -93.651759);
+		Vertex C = new Vertex(2874, 42.012025, -93.671429);
+		Vertex D = new Vertex(2351, 42.012053, -93.6489);
+		Vertex E = new Vertex(2956, 42.009345, -93.67229);
+		Vertex F = new Vertex(1171, 42.053516, -93.649586);
+		Vertex G = new Vertex(1208, 42.031262, -93.620315);
+		Vertex you = new Vertex(2893, 42.011189, -93.682421);
+		Edge Ab = new Edge(0, A, B, 1);
+		Edge Ae = new Edge(1, A, E, 1);
+		Edge Ag = new Edge(2, A, G, 1);
+		Edge Bc = new Edge(3, B, C, 1);
+		Edge Bd = new Edge(4, B, D, 1);
+		Edge Bg = new Edge(5, B, G, 1);
+		Edge Cg = new Edge(6, C, G, 1);
+		Edge De = new Edge(7, D, E, 1);
+		Edge Df = new Edge(8, D, F, 1);
+		Edge Dg = new Edge(9, D, G, 1);
+		Edge Eg = new Edge(10, E, G, 1);
+		Edge Fg = new Edge(11, F, G, 1);
+		Edge youA = new Edge(12, you, A, 1);
+		locations.addVertex(you);//0
+		locations.addVertex(A);//1
+		locations.addVertex(B);//2
+		locations.addVertex(C);//3
+		locations.addVertex(D);//4
+		locations.addVertex(E);//5
+		locations.addVertex(F);//6
+		locations.addVertex(G);//7
+		//locations.addVertex(you);//7
+		locations.addEdge(1, 2, Ab);
+		locations.addEdge(1, 5, Ae);
+		locations.addEdge(1, 7, Ag);
+		locations.addEdge(2, 3, Bc);
+		locations.addEdge(2, 4, Bd);
+		locations.addEdge(2, 7, Bg);
+		locations.addEdge(3, 7, Cg);
+		locations.addEdge(4, 5, De);
+		locations.addEdge(4, 6, Df);
+		locations.addEdge(4, 7, Dg);
+		locations.addEdge(5, 7, Eg);
+		locations.addEdge(6, 7, Fg);
+		locations.addEdge(0, 1, youA);
+		
+		return locations;
+	}
 
 }
